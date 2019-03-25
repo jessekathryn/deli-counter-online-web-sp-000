16b7f51549884acc9d4b6d9bf2d7bf628874279b
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-03-25 03:33:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,7 @@ def take_a_number(katz_deli, patron)
   
 def now_serving(line)
  if line.length >= 1
-  puts "Currently serving line.shift
+  puts "Currently serving #{line.shift}."
  else 
   puts "There is nobody waiting to be served!"
  end
